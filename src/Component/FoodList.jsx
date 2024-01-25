82cdf692d3c5151d5e539b537e8bb2d228d48b1c
Varinda Khanna <varinda.khanna@adw.kochartech.com>
2024-01-25 14:03:15 +0530

    props

--- src/Component/FoodList.jsx
+++ src/Component/FoodList.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import FoodItem from "./FoodItem";
 
 export default function FoodList({ item }) {
   //or directly write {item}
-  return (
-    <>
-      <div className="container pt-4">
-        <h3 className="pb-3">Food items</h3>
-        <ul className="list-group">
-          {item.map((x) => {
-           return <li className="list-group-item">{x}</li>;
-          })}
-        </ul>
+  if (item.length == 0) {
+    return (
+      <div className="alert alert-primary" role="alert">
+        No food item
       </div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div className="container pt-4">
+          <h3 className="pb-3">Food items</h3>
+          <ul className="list-group">
+            {item.map((x) => {
+              return (
+                <FoodItem x={x}/>
+              );
+            })}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
